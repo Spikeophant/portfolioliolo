749b607b302c0bc65e9664ed3539a2d285658c7f
Dustin Davisson <dustin.davisson@gmail.com>
2022-11-19 19:34:14 -0800

    Create Contact component

---
+++ src/component/Contact.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Contact() {
+  return(
+    <div>
+      <ul>
+        <li>
+          <a href="http://github.com/Spikeophant">GitHub</a>
+        </li>
+        <li>
+          <a href="mailto:dustin.davisson@gmail.com">Email</a>
+        </li>
+        <li>
+          <a href="https://instagram.com/spikeophant">Instagram</a>
+        </li>
+      </ul>
+    </div>
+)
+}
