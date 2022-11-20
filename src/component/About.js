91ddb498906ed5536665b4f7e1a8e57830198d2c
Dustin Davisson <dustin.davisson@gmail.com>
2022-11-19 19:33:05 -0800

    Create About component.

---
+++ src/component/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function About() {
+  return(
+    <div>
+    <p>
+      I'm Dustin Davisson, and am an SRE at Kohls.  I love what I do and the challengers of working at scale.  Automation of
+      of repeating tasks and toil, CI/CD, Security, and day two operations are what drive me.  I can write software and run it,
+      currently Staff SRE at Kohls, working in Infrastructure.  My current personal drives are post deploy automation, automated
+      validation and always driving towards deeper observability.  I'm nearing the end of a front end software engineering bootcamp,
+      and looking forward to my next learning journey.  A day not learning is a day wasted. Use the right tool for the job, and the platform where possible.
+    </p>
+  </div>
+  )
+}
