ed318941d2d7468fa8e3e22299a0b935429d80b5
Dustin Davisson <dustin.davisson@gmail.com>
2022-11-19 19:46:18 -0800

    Return Content finally.  Import bootstrap for styling, still need to figure out if we can use draculaui with bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './component/Nav';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Content from './component/Content';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="container" id="container">
+      <Content />
+    </section>
   );
 }
-
-export default App;
