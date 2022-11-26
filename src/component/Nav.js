178bd200432f36dc0740374d1cb6492a9f20c8f9
Dustin Davisson <dustin.davisson@gmail.com>
2022-11-26 14:30:40 -0800

    Updating nav for github pages.

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -7,13 +7,13 @@ export default function Nav({ handlePageChange }) {
       <a className='navbar-brand' href='/#about'>Dustin Davisson</a>
       <ul className='navbar-nav mr-auto'>
         <li className='nav-item'>
-          <a className='nav-link' href='/#about' onClick={() => handlePageChange('About')}>About Me</a>
+          <a className='nav-link' href='/#' onClick={() => handlePageChange('About')}>About Me</a>
         </li>
         <li className='nav-item'>
-          <a className='nav-link' href='/#portfolio' onClick={() => handlePageChange('Portfolio')}>Portfolio</a>
+          <a className='nav-link' href='/#' onClick={() => handlePageChange('Portfolio')}>Portfolio</a>
         </li>
         <li className='nav-item'>
-          <a className='nav-link' href='/#contact' onClick={() => handlePageChange('Contact')}>Contact</a>
+          <a className='nav-link' href='/#' onClick={() => handlePageChange('Contact')}>Contact</a>
         </li>
         <li className='nav-item'>
           <a className='nav-link' href='https://www.linkedin.com/in/dustin-davisson-32349897/'>Resume</a>
